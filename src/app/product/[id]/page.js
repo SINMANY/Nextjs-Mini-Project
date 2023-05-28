492309979bfe2386f389b0d996863c1baa2c58de
SINMANY <manysin48@gmail.com>
2023-05-28 20:49:24 +0700

    edit

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -39,7 +39,7 @@ export default async function ProductDetails({ params }) {
     const product = await getProductDetails(id);
     return (
 
-        <div class="flex flex-col items-center bg-gray-800 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <div class="flex flex-col items-center bg-gray-800 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 m-8">
             {product.images && (
                 <Image unoptimized width={500} height={500}
                     className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
