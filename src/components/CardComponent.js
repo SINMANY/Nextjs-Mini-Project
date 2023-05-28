c016542cce6563e6f939a11d96f61a897d003e43
SINMANY <manysin48@gmail.com>
2023-05-28 19:54:39 +0700

    edit

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -30,10 +30,11 @@ export default function CardComponent({ image, title, price, id }) {
                     <span class="text-3xl font-bold text-white dark:text-white">
                         ${price ? price : "Price"}
                     </span>
+                    <div class="flex items-center justify-between">
+                        <a href={`/product/${id}`} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Product Details</a>
+                    </div>
                 </div>
-                <div class="flex items-center justify-between">
-                    <a href={`/product/${id}`} class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Product Details</a>
-                </div>
+               
             </div>
         </div>
     )
